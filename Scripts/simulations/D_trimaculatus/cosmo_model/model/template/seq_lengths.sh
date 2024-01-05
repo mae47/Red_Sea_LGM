a9a63bfa314357eefcc85b401dbf7fe19bea012d
Maddie Emms <mae47@cam.ac.uk>
2024-01-05 21:19:39 +0000

    Added Dt simulations folder

---
+++ Scripts/simulations/D_trimaculatus/cosmo_model/model/template/seq_lengths.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#Checking max sequence lengths 
+#Splits .arp files from template/input_autosome folder which have sequences >1mil characters (not valid for arlsumstat later on)
+#All .arp files have different number of info lines at top so script checks length of last sequence line which is always 11 lines up from end of file
+#creates txt file to save sequence length of arp files and note which were split
+
+
+base_path="/home/mae47/rds/hpc-work/RAD_seq/D_trimaculatus/NRS_DGA/simulations/cosmo_model/island_1/model"
+
+cd $base_path/template/input_autosome/
+touch list_arp_lengths.txt
+
+
+
+for file in *.arp
+do
+#file=input_autosome_8_1.arp
+
+name=$(echo ${file:0:-6}) #filename prior to *_1.arp eg input_autosome_8
+lines=$(cat $file | wc -l) #length of file
+seqline=$(($lines - 11)) #line number of last sequence
+num=1  #file number counter either original arp file or split file
+
+
+while  [ $num -ge 1 ]
+do
+
+seqchar=$(sed -n $seqline'p' $file | wc -c) #number of characters in last sequence line (including line label tab) of original arp file
+
+	if [ $seqchar -gt 1000000 ]
+	then
+		if [ $num -eq 1 ]  #if this is the first split of this arp file, record it 
+		then
+		echo "$file sequence length $seqchar so split" >> list_arp_lengths.txt	
+		echo $file" sequence length > 1000000 ("$seqchar") so split in "$base_path >> ./../../../../list_split_arp_files.txt   #in root cosmo_model folder
+		echo $file" sequence length > 1000000 ("$seqchar") so split in "$base_path
+		fi
+	row=0
+	numplus=$(($num +1))
+	touch $name"_"$numplus".arp"  #make new split file
+		while read -r line  #NB -r ensures any backslashes are treated as regular characters
+		do
+		row=$(($row + 1))
+		if [ ${#line} -lt 1000001 ]  #short text lines
+		then
+		echo $line >> $name"_"$numplus".arp"
+		elif [ ${#line} -gt 1000000 ]  #too long sequences
+		then
+		#prefix=$(echo $line | awk '{print $1}')  #line label tab
+		#echo "line $prefix"
+		#echo "row $row split"
+		#print line label tab, space, and 1st million characters of sequence to a tempfile
+		echo $line | awk '{print $1,$2,substr($3,1,1000000)}' > $file"_temp.txt"
+		#print line label tab, space, and rest of sequence to new split file
+		echo $line | awk '{print $1,$2,substr($3,1000001)}' >> $name"_"$numplus".arp" 
+		tempfile=$file"_temp.txt"
+		#Following specific row number (Xr) of original $file, insert only label tab and 1st million characters as just printed in tempfile
+		sed -i -e "${row}r $tempfile" $file
+		#can now delete original line number with too long sequence
+		sed -i $row'd' $file
+		rm $tempfile
+		fi
+		done < $file
+	num=$(($num + 1))
+	file=$name"_"$numplus".arp"
+
+	else  #if file <1mil characters
+		if [ $num -eq 1 ]  #if this is the original arp file (not a new split file), record it 
+                then
+                echo "$file sequence length $seqchar" >> list_arp_lengths.txt       
+                fi
+	break  #exit 'while' loop
+	fi
+
+done #end of 'while' loop checking length of original arp file and each new split file
+                	
+
+done
+echo "finished looping all arp files, end of script checking seq lengths"
+ 
+
+
---
+++ Scripts/simulations/D_trimaculatus/cosmo_model/model/template/write_def.r
@@ -0,0 +1,46 @@
+####create def for fsc
+def_str<-c("/home/mae47/rds/hpc-work/RAD_seq/D_trimaculatus/NRS_DGA/simulations/cosmo_model/island_1/model/template/input_autosome.def") 
+
+
+#write header of the def file
+writeLines(paste("Nsource","Nsource_exp","Nanc","Nanc_exp","Nbott","Nbott_exp","Npop_RS","Npop_RS_exp","Npop_IO","Npop_IO_exp","split_IO","resize","resize_mod","tanc","tanc_exp","tbott","trec","tstop","tleng","gr","mig","mig_exp",sep="\t"), con=def_str, sep="\n")
+
+#All times are in generations: Dt 1 gen = 3 years
+for (y in 1:20){  #no. sims per folder
+    
+    npop_io<-runif(1,4,5.9) #10k to ~800k
+    nanc<-runif(1,4,5.5) #10k to ~300k
+    nsource_min<-log10(round(10^nanc) + round(10^npop_io))
+    repeat {
+	nsource<-runif(1,5.3,6.2) #~200k to ~1.6mil
+	if (nsource > nsource_min) {
+	break }	
+	}
+    	nbott<-runif(1,4,nanc) #10k to nanc
+	npop_rs<-runif(1,nbott,5.5) #nbott to ~300k
+
+	
+	trec<-runif(1,3833,5000) #time growth starts 11500ybp to 15000ybp
+	tlengmax<-trec-1 #ensures tstop > 0
+	tleng<-runif(1,50,tlengmax) #length of growth/growth period 150y to trec
+	tstop<-trec-tleng #end of growth
+	
+
+tbott<-runif(1,8000,12000) #24000-36000y
+tanc<-runif(1,4.125,5.301) #log, 13333-200000gens ie ~40000-600000y
+mig<-runif(1,-6.51,-2.98) #prop per gen, log, 0.0000003-0.00105gens ie 0.0000001 - 0.00035 per year or 0.00001% - 0.035%
+
+gr<-(log(round(10^nbott)/round(10^npop_rs)))/tleng #(popn before growth/popn after growth)/(time growth starts - time growth stops)
+split_io<-round(10^nsource)/round(10^npop_io)
+resize<-round(10^nanc)/round(10^nbott)
+resize_mod<-round(10^nbott)/round(10^npop_rs)
+
+#write set of parameters to the def file
+write(paste(round(10^nsource),nsource,round(10^nanc),nanc,round(10^nbott),nbott,round(10^npop_rs),npop_rs,round(10^npop_io),npop_io,split_io,resize,resize_mod,round(10^tanc),tanc,round(tbott),round(trec),round(tstop),round(tleng),gr,(10^mig),mig,sep="\t"), file=def_str, append=T)
+
+
+}
+
+
+
+
